278930fe7702e9a73b871e25b61720b1cc4782db
Curzy <cms9597@gmail.com>
2016-06-14 22:50:20 +0900

    make a phonecall to Danics Yang

--- Day1Homework/Day1Homework/View/D1DetailViewController.m
+++ Day1Homework/Day1Homework/View/D1DetailViewController.m
@@ -16,23 +16,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    CGRect screenBounds = [UIScreen mainScreen].bounds;
     
     self.view.backgroundColor =[UIColor grayColor];
     
     UILabel *label = [[UILabel alloc] init];
-    label.frame = CGRectMake(0, 20, self.view.frame.size.width, 40);
+    label.frame = CGRectMake(5, 70, self.view.frame.size.width, 40);
     label.text = @"Hello Chisoo!";
     label.textColor = [UIColor redColor];
     [label sizeToFit];
     
     UIImageView *chisoo = [[UIImageView alloc]init];
-    chisoo.frame = CGRectMake(0, label.frame.origin.y + label.frame.size.height, chisoo.image.size.width, chisoo.image.size.height);
     chisoo.image = [UIImage imageNamed: @"LAman.jpg"];
+    chisoo.frame = CGRectMake(5, label.frame.origin.y + label.frame.size.height, chisoo.image.size.width, chisoo.image.size.height);
+    
+    UILabel *phone = [[UILabel alloc]init];
+    phone.frame = CGRectMake(5, chisoo.frame.origin.y + chisoo.frame.size.height, self.view.frame.size.width, 40);
+    phone.text = @"+1 (310) 208-9679";
+    phone.textColor = [UIColor blueColor];
+    [phone sizeToFit];
+    
+    UIButton *makeCall = [[UIButton alloc] init];
+    makeCall.frame = CGRectMake(5, phone.frame.origin.y + phone.frame.size.height, self.view.frame.size.width, 40);
+    makeCall.backgroundColor = [UIColor blueColor];
+    [makeCall setTitle:@"Call Me!" forState:UIControlStateNormal];
+    [makeCall setTitle:@"Calling!" forState:UIControlStateHighlighted];
+    [makeCall addTarget:self action:@selector(makingCall:) forControlEvents:UIControlEventTouchUpInside];
     
     [self.view addSubview:label];
     [self.view addSubview:chisoo];
+    [self.view addSubview:phone];
+    [self.view addSubview:makeCall];
     // Do any additional setup after loading the view.
+}
+
+- (void) makingCall :(UIButton *) makeCall {
+    NSLog(@"전화 건다");
+    NSURL *url= [NSURL URLWithString:@"tel:+1 (310) 208-9679"];
+    [[UIApplication sharedApplication] openURL:url];
+
 }
 
 - (void)didReceiveMemoryWarning {
